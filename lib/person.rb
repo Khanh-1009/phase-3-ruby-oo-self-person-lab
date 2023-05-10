87f337aa4a07dc9451edba188c2c00656c96682a
Khanh Tran <katetrannbkhanh@gmail.com>
2023-05-09 21:19:20 -0700

    commit tree 68675a818d9174e82c4fe0942e16c7597093538b

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,42 @@
 # your code goes here
+require 'pry'
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(value)
+        @happiness = if value > 10
+            10
+        elsif value < 0
+            0
+        else
+            value
+        end
+    end
+
+    def hygiene=(value)
+        @hygiene = if value > 10
+            10
+        elsif value < 0
+            0
+        else
+            value
+        end
+    end
+
+    def happy?
+        if self.happiness > 7
+            true
+        else
+            false
+        end
+    end
+    
+end
